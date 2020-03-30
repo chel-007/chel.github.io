09cbeb84adce690fb32107fe4e460e5bd006f65f
Chel-Ash <58524269+Chel-Ash@users.noreply.github.com>
2020-03-30 19:20:47 +0200

    Update new.js

--- new.js
+++ new.js
@@ -1,33 +1,15 @@
-if (typeof jQuery != 'undefined') { // jQuery is loaded => print the version alert(jQuery.fn.jquery); } 
-function AddReadMore() {
-    //This limit you can set after how much characters you want to show Read More.
-    var carLmt = 20;
-    // Text to show when text is collapsed
-    var readMoreTxt = " ... Read More";
-    // Text to show when text is expanded
-    var readLessTxt = " Read Less";
+var myvid = document.getElementById('myvideo');
+var myvids = [
+  "http://www.w3schools.com/html/mov_bbb.mp4", 
+  "http://www.w3schools.com/html/movie.mp4"
+  ];
+var activeVideo = 0;
 
+myvid.addEventListener('ended', function(e) {
+  // update the new active video index
+  activeVideo = (++activeVideo) % myvids.length;
 
-    //Traverse all selectors with this class and manupulate HTML part to show Read More
-    $(".addReadMore").each(function() {
-        if ($(this).find(".firstSec").length)
-            return;
-
-        var allstr = $(this).text();
-        if (allstr.length > carLmt) {
-            var firstSet = allstr.substring(0, carLmt);
-            var secdHalf = allstr.substring(carLmt, allstr.length);
-            var strtoadd = firstSet + "<span class='SecSec'>" + secdHalf + "</span><span class='readMore'  title='Click to Show More'>" + readMoreTxt + "</span><span class='readLess' title='Click to Show Less'>" + readLessTxt + "</span>";
-            $(this).html(strtoadd);
-        }
-
-    });
-    //Read More and Read Less Click Event binding
-    $(document).on("click", ".readMore,.readLess", function() {
-        $(this).closest(".addReadMore").toggleClass("showlesscontent showmorecontent");
-    });
-}
-$(function() {
-    //Calling function after Page Load
-    AddReadMore();
+  // update the video source and play
+  myvid.src = myvids[activeVideo];
+  myvid.play();
 });
