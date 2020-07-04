d7928a8851a11fe85cbd22e4d189f166f3697587
ChelAsh <wstones025@gmail.com>
2020-07-04 23:52:54 +0100

    Moved a few scripts to one combined file

---
+++ assets/js/defer.js
@@ -0,0 +1,17 @@
+
+<script async data-ad-client="ca-pub-3206299196679214" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+<script async src="https://cdn.jsdelivr.net/npm/instantsearch.js@4.0.0/dist/instantsearch.production.min.js" integrity="sha256-6S7q0JJs/Kx4kb/fv0oMjS855QTz5Rc2hh9AkIUjUsk=" crossorigin="anonymous"></script>
+
+<script async>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1694485,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
+<link href="https://fonts.googleapis.com/css?family=Rubik|Roboto+Mono|Balsamiq+Sans&display=swap" rel="stylesheet" onload="this.media='all'; this.onload=null;">
